1cc1ef3d1ac7d7eae91e8c200fe4472ced5ed203
Arkan Arsalan <arkan.arsalan.amanullah@gmail.com>
2024-05-14 22:52:44 +0700

    feat: add date API endpoint to handle date parsing and formatting

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -18,13 +18,32 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+// API endpoint
+app.get('/api/:date?', (req, res) => {
+  let dateInput = req.params.date;
+  let date;
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  // if no date provided in paramater
+  if (!dateInput) {
+    date = new Date();
+  } else {
+    // Check if the input is unix timestamp
+    if (!isNaN(dateInput)) {
+      dateInput = parseInt(dateInput);
+    }
+    date = new Date(dateInput);
+  }
+
+  // Check if the date is valid
+  if (!isNaN(date.getTime())) {
+    res.json({
+      unix: date.getTime(),
+      utc: date.toUTCString()
+    });
+  } else {
+    res.json({ error: "Invalid Date" });
+  }
 });
-
-
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
